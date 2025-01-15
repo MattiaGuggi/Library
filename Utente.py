f4f0086ab96b838b245a7b8229938c25b3b1db30
fastus201 <fabiobotta4@gmail.com>
2025-01-15 10:14:15 +0100

    add 2 methods

--- Utente.py
+++ Utente.py
@@ -3,5 +3,11 @@ class Utente:
     def __init__(self,nome):
         self.nome = nome
         self.libri_prestiti = []
+
+    #Metodo per aggiungere un libro alla lista di quelli in prestito
+    def prendi_libro(self,libro):
+        self.libri_prestiti.append(libro)
     
-   
+    #Metodo per restituire un libro alla biblioteca
+    def restituisci_libro(self,libroDaTogliere):
+        self.libri_prestiti.remove(libroDaTogliere)
