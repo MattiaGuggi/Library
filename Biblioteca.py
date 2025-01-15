dd924c8ec4e3b269e44a44b8876754f9bb550d84
GUGGI MATTIA HA <MA.GUGGI@Magistri.local>
2025-01-15 10:41:34 +0100

    ok

--- Biblioteca.py
+++ Biblioteca.py
@@ -3,23 +3,37 @@ class Biblioteca:
         self.lista_utenti = lista_utenti
         self.lista_libri = lista_libri
         
-    def aggiungi_libro(self):
+    def aggiungi_libro(self, libro):
+        self.lista_libri.append(libro)
         print('Aggiungi un libro')
     
-    def aggiungi_utente(self):
-        print('Aggiungi un utente')
+    def aggiungi_utente(self, utente):
+        self.lista_utenti.append(utente)
     
-    def cerca_libro(self):
+    def cerca_libro(self, title):
         print('Cerca un libro')
+        for libro in self.lista_libri:
+            if libro.titolo == title:
+                return True
+            
+        return False
     
-    def prestito(self):
+    def prestito(self, utente, title, author, year):
         print('Prestito')
+        for libro in self.lista_libri:
+            if libro.titolo == title and libro.autore == author and libro.anno_pubbl == year:
+                self.lista_libri.remove(libro)
+                utente.prendi_libro(libro)
+                return True
+            
+        return False
     
-    def restituzione(self):
+    def restituzione(self, utente, title, author, year):
         print('Restituisci')
-        
-def main():
-    biblioteca = Biblioteca([], [])
-    
-if __name__ == '__main__':
-    main()
+        for libro in self.lista_libri:
+            if libro.titolo == title and libro.autore == author and libro.anno_pubbl == year:
+                self.lista_libri.append(libro)
+                utente.restituisci_libro(libro)
+                return True
+            
+        return False
