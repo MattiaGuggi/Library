8bcb189c9f25922d8f1e821560f6de0aa3da5359
MattiaGuggi <mattiahag@gmail.com>
2025-01-15 15:21:20 +0100

    Aggiunta lista libri

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,17 +1,36 @@
+from Libro import Libro # type: ignore
+
 class Biblioteca:
     def __init__(self, lista_utenti, lista_libri):
         self.lista_utenti = lista_utenti
         self.lista_libri = lista_libri
         
+    def crea_libri(self):
+        self.lista_libri = [
+            Libro("1984", "George Orwell", 1949),
+            Libro("Il Signore degli Anelli", "J.R.R. Tolkien", 1954),
+            Libro("Orgoglio e Pregiudizio", "Jane Austen", 1813),
+            Libro("Il Piccolo Principe", "Antoine de Saint-Exupéry", 1943),
+            Libro("Moby Dick", "Herman Melville", 1851),
+            Libro("Il Nome della Rosa", "Umberto Eco", 1980),
+            Libro("La Divina Commedia", "Dante Alighieri", 1320),
+            Libro("Harry Potter e la Pietra Filosofale", "J.K. Rowling", 1997),
+            Libro("Il Grande Gatsby", "F. Scott Fitzgerald", 1925),
+            Libro("Cime Tempestose", "Emily Brontë", 1847),
+            Libro("Don Chisciotte della Mancia", "Miguel de Cervantes", 1605),
+            Libro("Guerra e Pace", "Lev Tolstoj", 1869),
+            Libro("Anna Karenina", "Lev Tolstoj", 1877),
+            Libro("Il Processo", "Franz Kafka", 1925),
+            Libro("Le Cronache di Narnia", "C.S. Lewis", 1950)
+        ]
+        
     def aggiungi_libro(self, libro):
         self.lista_libri.append(libro)
-        print('Aggiungi un libro')
     
     def aggiungi_utente(self, utente):
         self.lista_utenti.append(utente)
     
     def cerca_libro(self, title):
-        print('Cerca un libro')
         for libro in self.lista_libri:
             if libro.titolo == title:
                 return True
@@ -19,7 +38,6 @@ class Biblioteca:
         return False
     
     def prestito(self, utente, title, author, year):
-        print('Prestito')
         for libro in self.lista_libri:
             if libro.titolo == title and libro.autore == author and libro.anno_pubbl == year:
                 self.lista_libri.remove(libro)
