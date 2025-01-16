484daba61c29c8bec3623125c81a718d02e8ce42
pcto_stedino <tedinosimone06@gmail.com>
2025-01-16 17:03:45 +0100

    Completamento file Interfaccia.py

--- Interfaccia.py
+++ Interfaccia.py
@@ -3,52 +3,96 @@ from Utente import Utente
 from Biblioteca import Biblioteca
 
 class Interfaccia:
-    
+
+    def __init__(self):
+        self.biblioteca = Biblioteca()
+
     def menu_principale(self):
-        print("1)Aggiungi un nuovo libro")
-        print("2)Aggiungi un nuovo utente")
-        print("3)Cerca un libro nella biblioteca")
-        print("4)Prendi in prestito un libro")
-        print("5)Restituisci un libro")
-        scelta = int(input("Scegli un 'opzione: "))
-        while scelta < 1 or scelta > 5:
-            print("Scegli un'opzione valida")
-            scelta = int(input("Scegli un 'opzione: "))
-        if scelta == 1:
-            self.aggiungi_libro_interattivo()
-        elif scelta == 2:
-            self.aggiungi_utente()
-        elif scelta == 3:
-            self.cerca_libro()
-        elif scelta == 4:
-            self.prendi_in_presto()
-        elif scelta == 5:
-            self.restituisci_libro()
-    
-    
+        while True:
+            print("\nMenu principale:")
+            print("1)Aggiungi un nuovo libro")
+            print("2)Aggiungi un nuovo utente")
+            print("3)Cerca un libro nella biblioteca")
+            print("4)Prendi in prestito un libro")
+            print("5)Restituisci un libro")
+            print("6)Esci")
+            scelta = int(input("Scegli un'opzione: "))
+            
+            if scelta == 1:
+                self.aggiungi_libro_interattivo()
+            elif scelta == 2:
+                self.aggiungi_utente()
+            elif scelta == 3:
+                self.cerca_libro()
+            elif scelta == 4:
+                self.prendi_in_prestito()
+            elif scelta == 5:
+                self.restituisci_libro()
+            elif scelta == 6:
+                print("Uscita dal programma")
+                break
+            else:
+                print("Opzione non valida, riprova.")
+
     def aggiungi_libro_interattivo(self):
         titolo = input("Inserisci il titolo del libro: ")
         autore = input("Inserisci l'autore del libro: ")
-        data_pubblicazione = int(input("Inserisci la data di pubblicazione del libro"))
-        libro = Libro(titolo, autore, data_pubblicazione)
+        anno_pubblicazione = int(input("Inserisci l'anno di pubblicazione del libro: "))
+        libro = Libro(titolo, autore, anno_pubblicazione)
+        self.biblioteca.aggiungi_libro(self.biblioteca, libro)
 
     def aggiungi_utente(self):
         nome = input("Inserisci il nome dell'utente: ")
-        libri_prestiti = []
-        autore = Autore(nome, libri_prestiti)
+        utente = Utente(nome)
+        self.biblioteca.aggiungi_utente(self.biblioteca, utente)
 
     def cerca_libro(self):
-        titolo = input("Inserisci il titolo del libro che vuoi cercare:")
+        titolo = input("Inserisci il titolo del libro che vuoi cercare: ")
+        trovato = self.biblioteca.cerca_libro(self.biblioteca, titolo)
+        if trovato:
+            print("Il libro è stato trovato.")
+        else:
+            print("Il libro non è presente nella biblioteca")
 
     def prendi_in_presto(self):
-        titolo = input("Inserisci il titolo del libro: ")
+        trovato = False
+        nome_utente = input("inserire il nome dell'utente: ")
+        titolo = input("Inserisci il titolo del libro che vuoi prendere in prestito: ")
         autore = input("Inserisci l'autore del libro: ")
-        data_pubblicazione = int(input("Inserisci la data di pubblicazione del libro"))
-    
+        anno_pubblicazione = int(input("Inserisci l'anno di pubblicazione del libro: "))
+        libro = Libro(titolo, autore, anno_pubblicazione)
+        for utente in self.biblioteca.lista_utenti:
+            if utente.nome == nome_utente:
+                trovato = True
+                preso = self.biblioteca.prestito(self.biblioteca, utente, titolo, autore, anno_pubblicazione)
+                if preso:
+                    print("Libro preso in prestito")
+                else:
+                    print("Errore nella presa in prestito del libro")
+                break
+        if trovato == False:
+            print("utente non trovato")
+
     def restituisci_libro(self):
-        titolo = input("Inserisci il titolo del libro: ")
+        trovato = False
+        nome_utente = input("inserire il nome dell'utente: ")
+        titolo = input("Inserisci il titolo del libro che vuoi restituire: ")
         autore = input("Inserisci l'autore del libro: ")
-        data_pubblicazione = int(input("Inserisci la data di pubblicazione del libro"))
+        anno_pubblicazione = int(input("Inserisci l'anno di pubblicazione del libro: "))
+        libro = Libro(titolo, autore, anno_pubblicazione)
+        for utente in self.biblioteca.lista_utenti:
+            if utente.nome == nome_utente:
+                trovato = True
+                restituito = self.biblioteca.restituzione(self.biblioteca, utente, titolo, autore, anno_pubblicazione)
+                if restituito:
+                    print("Libro restituito")
+                else:
+                    print("Errore nella restituzione del libro")
+                break
+        if trovato == False:
+            print("utente non trovato")
 
 
-    
+if __name__ == "__main__":
+    interfaccia = Interfaccia()
+    interfaccia.menu_principale()
